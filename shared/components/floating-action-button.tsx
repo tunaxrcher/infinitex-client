12978242bd794902d8f4c30c38434e1b1d2a23f7
tunaxrcher <mystic.outriderxpaine@gmail.com>
2025-09-25 18:31:43 +0700

    .

--- shared/components/floating-action-button.tsx
+++ shared/components/floating-action-button.tsx
@@ -1,5 +1,5 @@
 import { Plus } from "lucide-react"
-import { Button } from "@/features/shared/ui/button"
+import { Button } from "@/shared/ui/button"
 import Link from "next/link"
 
 export function FloatingActionButton() {
